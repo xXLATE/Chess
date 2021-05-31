714c776a364fe0b3e3f0934882072fe14891c372
LalPotRash <daniil.kanev1994@gmail.com>
2021-05-31 17:37:45 +0300

    make working chess board

--- WPFChess/MainWindow.xaml.cs
+++ WPFChess/MainWindow.xaml.cs
@@ -21,9 +21,59 @@ namespace WPFChess
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ImageBrush brush;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void KingBtn_Click(object sender, RoutedEventArgs e)
+        {
+            brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/King.png"));
+        }
+
+        private void QueenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/Queen.png"));
+        }
+
+        private void RookBtn_Click(object sender, RoutedEventArgs e)
+        {
+            brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/Rook.png"));
+        }
+
+        private void BishopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/Bishop.png"));
+        }
+
+        private void KnightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/Knight.png"));
+        }
+
+        private void PawnBtn_Click(object sender, RoutedEventArgs e)
+        {
+            brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/Pawn.png"));
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Field_Click(object sender, RoutedEventArgs e)
+        {
+            Control control = (Control)sender;
+            Button button = (Button)sender;
+            button.Content = new Rectangle { Fill = brush, Width=75, Height=75};
         }
     }
 }
